e5924ced559f3b36e2fbdc62bfbce9d3769e7966
Biswaranjan Pradhan <biswaranjanpradhan287@gmail.com>
2026-01-06 14:15:32 +0530

    feat(client): enhance JobListing with job filtering options and add JobCard component for job display

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
-import { assets } from '../assets/assets'
+import { assets, JobCategories, JobLocations } from '../assets/assets'
+import JobCard from './JobCard'
 
 const JobListing = () => {
 
-    const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext)
+    const { isSearched, searchFilter, setSearchFilter, jobs } = useContext(AppContext)
 
     return (
         <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
@@ -22,7 +23,7 @@ const JobListing = () => {
                                     <span className='inline-flex items-center gap-2.5 bg-blue-50 border border-blue-200 px-4 py-1.5 rounded'>
                                         {searchFilter.title}
                                         <img
-                                            onClick={e => setSearchFilter(prev => ({ ...prev, title:"" }))}
+                                            onClick={e => setSearchFilter(prev => ({ ...prev, title: "" }))}
                                             className='cursor-pointer'
                                             src={assets.cross_icon} alt=""
                                         />
@@ -32,7 +33,7 @@ const JobListing = () => {
                                     <span className='ml-2 inline-flex items-center gap-2.5 bg-red-50 border border-red-200 px-4 py-1.5 rounded'>
                                         {searchFilter.location}
                                         <img
-                                            onClick={e => setSearchFilter(prev => ({ ...prev, location:"" }))}
+                                            onClick={e => setSearchFilter(prev => ({ ...prev, location: "" }))}
                                             className='cursor-pointer'
                                             src={assets.cross_icon} alt=""
                                         />
@@ -43,7 +44,48 @@ const JobListing = () => {
                     )
                 }
 
+                {/* Category Filter */}
+                <div className='max-lg:hidden'>
+                    <h4 className='font-medium text-lg py-4'>Search by Categories</h4>
+                    <ul className='space-y-4 text-gray-600'>
+                        {
+                            JobCategories.map((category, index) => (
+                                <li className='flex gap-3 items-center' key={index}>
+                                    <input className='scale-125' type="checkbox" name="" id="" />
+                                    {category}
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+
+                {/* Location Filter */}
+                <div className='max-lg:hidden'>
+                    <h4 className='font-medium text-lg py-4 pt-14'>Search by Location</h4>
+                    <ul className='space-y-4 text-gray-600'>
+                        {
+                            JobLocations.map((location, index) => (
+                                <li className='flex gap-3 items-center' key={index}>
+                                    <input className='scale-125' type="checkbox" name="" id="" />
+                                    {location}
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+
             </div>
+
+            {/* Job Listing */}
+            <section className='w-full lg:h-3/4 text-gray-800 max-lg:px-4'>
+                <h3 className='font-medium text-3xl py-2' id='job-list'>Latest Jobs</h3>
+                <p className='mb-8'>Get your desired job from top companies</p>
+                <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
+                    {jobs.map((job, index) => (
+                        <JobCard key={index} job={job} />
+                    ))}
+                </div>
+            </section>
 
         </div>
     )

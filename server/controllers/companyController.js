957ec267b2f9490efdfef1af8b81fb4708319fa0
Biswaranjan Pradhan <biswaranjanpradhan287@gmail.com>
2026-01-17 15:39:08 +0530

    feat(company): implement company controller with initial methods

---
+++ server/controllers/companyController.js
@@ -0,0 +1,40 @@
+
+/* Register a new company */
+export const registerCompany = async (req, res) => {
+    
+}
+
+/* Company login */
+export const loginCompany = async (req, res) => {
+    
+}
+
+/* Get company data */
+export const getCompanyData = async (req, res) => {
+    
+}
+
+/* Post a new job */
+export const postJob = async (req, res) => {
+    
+}
+
+/* Get company job applicants */
+export const getCompanyJobApplicants = async (req, res) => {
+    
+}
+
+/* Get company posted jobs */
+export const getCompanyPostedJobs = async (req, res) => {
+    
+}
+
+/* Change job applications status */
+export const changeJobApplicationsStatus = async (req, res) => {
+    
+}
+
+/* Change job visiblity */
+export const changeVisiblity = async (req, res) => {
+    
+}

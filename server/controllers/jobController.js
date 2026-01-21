8dd472b1553809fcc01f62071c42d99c9567601c
Biswaranjan Pradhan <biswaranjanpradhan287@gmail.com>
2026-01-21 17:40:36 +0530

    feat: get job by id with company details

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -12,11 +12,35 @@ export const getJobs = async (req, res) => {
         res.json({ success: true, jobs })
 
     } catch (error) {
-        res.json({success:false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
 /* Get a single job by id */
 export const getJobById = async (req, res) => {
+    try {
 
+        const { id } = req.params
+
+        const job = await Job.findById(id)
+            .populate({
+                path: 'companyId',
+                select: '-password'
+            })
+
+        if (!job) {
+            return res.json({
+                success: false,
+                message: 'Job not found'
+            })
+        }
+
+        res.json({
+            success: true,
+            job
+        })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
 }

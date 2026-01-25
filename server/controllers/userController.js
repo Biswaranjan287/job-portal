562084eea19bed2145601ddacd5e83f8a13c8f68
Biswaranjan Pradhan <biswaranjanpradhan287@gmail.com>
2026-01-25 17:38:10 +0530

    feat: add get user data API using Clerk auth

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,24 @@
+import User from "../models/User.js"
 
 
 /* Get user data */
 export const getUserData = async (req, res) => {
+
+    const userId = req.auth.userId
+
+    try {
+
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return res.json({ success: false, message: 'User Not Found' })
+        }
+
+        res.json({ success: true, user })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
 
 }
 

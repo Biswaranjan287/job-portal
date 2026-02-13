847ce3b4b51d380bdff4c53859b7e411abfea072
Biswaranjan Pradhan <biswaranjanpradhan287@gmail.com>
2026-02-13 18:50:28 +0530

    feat: implement job fetching functionality in ManageJobs component

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { manageJobsData } from '../assets/assets'
 import moment from 'moment'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 const ManageJobs = () => {
 
     const navigate = useNavigate()
-    
+
+    const [jobs, setJobs] = useState(false)
+
+    const { backendUrl, companyToken } = useContext(AppContext)
+
+    /* Function to fetch company job applications data */
+    const fetchCompanyJobs = async () => {
+
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/company/list-jobs',
+                { headers: { token: companyToken } }
+            )
+
+            if (data.success) {
+                setJobs(data.jobsData.reverse())
+                console.log(data.jobsData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+
+    }
+
+    useEffect(() => {
+        if (companyToken) {
+            fetchCompanyJobs()
+        }
+    }, [companyToken])
+
     return (
         <div className='container p-4 max-w-5xl'>
             <div className='overflow-auto'>
@@ -22,9 +57,9 @@ const ManageJobs = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {manageJobsData.map((job, index)=>(
+                        {manageJobsData.map((job, index) => (
                             <tr key={index} className='text-gray-700'>
-                                <td className='py-2 px-4 border-b border-gray-200 max-sm:hidden'>{index+1}</td>
+                                <td className='py-2 px-4 border-b border-gray-200 max-sm:hidden'>{index + 1}</td>
                                 <td className='py-2 px-4 border-b border-gray-200'>{job.title}</td>
                                 <td className='py-2 px-4 border-b border-gray-200 max-sm:hidden'>{moment(job.date).format('ll')}</td>
                                 <td className='py-2 px-4 border-b border-gray-200 max-sm:hidden'>{job.location}</td>
@@ -38,7 +73,7 @@ const ManageJobs = () => {
                 </table>
             </div>
             <div className='mt-4 flex justify-end'>
-                <button onClick={()=> navigate('/dashboard/add-job')} className='bg-black text-white py-2 px-4 rounded'>Add new job </button>
+                <button onClick={() => navigate('/dashboard/add-job')} className='bg-black text-white py-2 px-4 rounded'>Add new job </button>
             </div>
         </div>
     )
